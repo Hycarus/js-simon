54907e7ed816d9dafe21190728e437bb4f93f5e6
Hycarus <mirkomarasco08@gmail.com>
2023-10-20 16:51:22 +0200

    try to create reset button

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 (function (){
-    const btn = document.querySelector('button');
+    const btn = document.getElementById('start');
+    const btn2 = document.getElementById('reset');
     const wrapper = document.getElementById('wrapper');
     const memory = 5;
     const input = document.getElementById('input');
-    const inputNumber = document.getElementById('input-number');
+    const inputNumber = document.querySelectorAll('input');
     // const timerEl = document.querySelector('.timer');
     // const clock = setInterval(timer, 1000);
     let randomNumberArray = [];
     let userNumberArray = [];
-
+    let checkArray = [];
     generateRndNumber();
     wrapper.innerHTML = randomNumberArray;
 
@@ -18,20 +19,49 @@
     }, 1000);
 
     btn.addEventListener('click', function(){
-        userNumberArray.push(inputNumber.value);
+        for(let i = 0; i < inputNumber.length; i++){
+            if(!isNaN(parseInt(inputNumber[i].value))){
+                userNumberArray.push(parseInt(inputNumber[i].value));
+            }
+            if(randomNumberArray[i] === userNumberArray[i]){
+                checkArray.push(userNumberArray[i]);
+            }
+        }
         console.log(userNumberArray);
-        inputNumber.value = '';
+        btn.classList.add('d-none');
+        input.classList.add('d-none');
+        btn2.classList.remove('d-none');
     })
 
+    btn2.addEventListener('click', function(){
+        btn.classList.remove('d-none');
+        input.classList.remove('d-none');
+        btn2.classList.add('d-none');
+        inputNumber.value = '';
+    })  
     
+    console.log(randomNumberArray);
+    // function check(){
+        
+    //     for(let i = 0; i < randomNumberArray.length; i++){
+    //         if(userNumberArray.includes(randomNumberArray[i])){
+    //             checkArray.push(userNumberArray);
+    //             console.log(checkArray);
+    //             console.log('you did it');
+    //         } else{
+    //             console.log('suca');
+    //         }
+    //     }
+        
+    // }
+
     function generateRndNumber(){
         while(randomNumberArray.length < memory){
             let number = getRndInteger(1, 1000);
             if(!randomNumberArray.includes(number)){
-                randomNumberArray.push(number);
+                randomNumberArray.push(parseInt(number));
             }
         }
-        console.log(randomNumberArray);
         return randomNumberArray;
     }
 
